566c5c21ac21305499d1abf3eb2e070276fc05b1
libin <libinjob@163.com>
2023-03-11 21:48:56 +0800

    fix：debug

--- debugUtil/dd.go
+++ debugUtil/dd.go
@@ -1,12 +1,7 @@
 package debugUtil
 
-import (
-	"fmt"
-	"os"
-)
-
 // DD Debug打断调试
-func DD(value ...interface{}) {
-	fmt.Println(value)
-	os.Exit(1)
-}
+//func DD(value ...interface{}) {
+//	fmt.Println(value)
+//	os.Exit(1)
+//}
