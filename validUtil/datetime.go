afd6bc1436ec83fc93403ba25f1453955f0c2fbc
libin <libinjob@163.com>
2023-03-12 13:44:02 +0800

    优化代码复杂度

--- validUtil/datetime.go
+++ validUtil/datetime.go
@@ -3,6 +3,7 @@ package validUtil
 import (
 	"regexp"
 	"strconv"
+	"strings"
 )
 
 // IsTime 验证是否为时间格式（HH:mm:ss）
@@ -17,37 +18,50 @@ func IsDate(str string) bool {
 	if !reg.MatchString(str) {
 		return false
 	}
+	parts := strings.Split(str, "-")
+	year, _ := strconv.Atoi(parts[0])
+	month, _ := strconv.Atoi(parts[1])
+	day, _ := strconv.Atoi(parts[2])
+	return isValidMonth(month, year) && isValidDay(day, month, year)
+}
 
-	year, _ := strconv.Atoi(str[0:4])
-	month, _ := strconv.Atoi(str[5:7])
-	day, _ := strconv.Atoi(str[8:10])
-
-	if month < 1 || month > 12 || day < 1 {
-		return false
-	}
-
+func isValidMonth(month, year int) bool {
 	switch month {
+	case 1, 3, 5, 7, 8, 10, 12:
+		return true
+	case 4, 6, 9, 11:
+		return false
 	case 2:
 		if year%4 == 0 && year%100 != 0 || year%400 == 0 {
-			if day > 29 {
-				return false
-			}
-		} else {
-			if day > 28 {
-				return false
-			}
+			return true
+		}
+		return false
+	}
+	return false
+}
+
+func isValidDay(day, month, year int) bool {
+	switch month {
+	case 1, 3, 5, 7, 8, 10, 12:
+		if day >= 1 && day <= 31 {
+			return true
 		}
 	case 4, 6, 9, 11:
-		if day > 30 {
-			return false
+		if day >= 1 && day <= 30 {
+			return true
 		}
-	default:
-		if day > 31 {
-			return false
+	case 2:
+		if year%4 == 0 && year%100 != 0 || year%400 == 0 {
+			if day >= 1 && day <= 29 {
+				return true
+			}
+		} else {
+			if day >= 1 && day <= 28 {
+				return true
+			}
 		}
 	}
-
-	return true
+	return false
 }
 
 // IsDateTime 验证是否为日期时间格式（yyyy-MM-dd HH:mm:ss）
