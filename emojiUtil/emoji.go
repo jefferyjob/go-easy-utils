8aeca47564dacb5af204ea4ba3a8ce64a79ebae4
libin <libinjob@163.com>
2023-03-20 23:57:40 +0800

    emoji表情处理

--- emojiUtil/emoji.go
+++ emojiUtil/emoji.go
@@ -1,3 +1,4 @@
+// Package emojiUtil emoji表情处理工具
 package emojiUtil
 
 import (
@@ -6,27 +7,29 @@ import (
 	"strings"
 )
 
-// UnicodeEmojiDecode Emoji表情解码
-func UnicodeEmojiDecode(s string) string {
+// DecodeEmojiUnicode Emoji表情解码
+// 输入unicode编码，得到emoji表情
+func DecodeEmojiUnicode(unicode string) string {
 	//emoji表情的数据表达式
 	re := regexp.MustCompile("\\[[\\\\u0-9a-zA-Z]+\\]")
 	//提取emoji数据表达式
 	reg := regexp.MustCompile("\\[\\\\u|]")
-	src := re.FindAllString(s, -1)
+	src := re.FindAllString(unicode, -1)
 	for i := 0; i < len(src); i++ {
 		e := reg.ReplaceAllString(src[i], "")
 		p, err := strconv.ParseInt(e, 16, 32)
 		if err == nil {
-			s = strings.Replace(s, src[i], string(rune(p)), -1)
+			unicode = strings.Replace(unicode, src[i], string(rune(p)), -1)
 		}
 	}
-	return s
+	return unicode
 }
 
-// UnicodeEmojiCode Emoji表情转换
-func UnicodeEmojiCode(s string) string {
+// EncodeEmojiUnicode Emoji表情编码
+// 输入emoji表情，得到unicode编码
+func EncodeEmojiUnicode(emoji string) string {
 	ret := ""
-	rs := []rune(s)
+	rs := []rune(emoji)
 	for i := 0; i < len(rs); i++ {
 		if len(string(rs[i])) == 4 {
 			u := `[\u` + strconv.FormatInt(int64(rs[i]), 16) + `]`
--- emojiUtil/emoji_test.go
+++ emojiUtil/emoji_test.go
@@ -1,39 +1,88 @@
 package emojiUtil
 
-//func TestUnicodeEmojiDecode(t *testing.T) {
-//	testCases := []struct {
-//		input    string
-//		expected string
-//	}{
-//		{"Hello [\\ud83d\\ude0a] [\\ud83d\\ude0b]", "Hello 😊 😋"},
-//		{"[\\ud83d\\ude0a]", "😊"},
-//		{"[\\ud83d\\ude0a]123[\\ud83d\\ude0b]456", "😊123😋456"},
-//		{"No emoji", "No emoji"},
-//	}
-//
-//	for _, tc := range testCases {
-//		actual := UnicodeEmojiDecode(tc.input)
-//		if actual != tc.expected {
-//			t.Errorf("Unexpected result - input: %s, expected: %s, got: %s", tc.input, tc.expected, actual)
-//		}
-//	}
-//}
+import (
+	"strings"
+	"testing"
+)
 
-//func TestUnicodeEmojiCode(t *testing.T) {
-//	testCases := []struct {
-//		input    string
-//		expected string
-//	}{
-//		{"Hello 😊 😋", "Hello [\\ud83d\\ude0a] [\\ud83d\\ude0b]"},
-//		{"😊", "[\\ud83d\\ude0a]"},
-//		{"😊123😋456", "[\\ud83d\\ude0a]123[\\ud83d\\ude0b]456"},
-//		{"No emoji", "No emoji"},
-//	}
-//
-//	for _, tc := range testCases {
-//		actual := UnicodeEmojiCode(tc.input)
-//		if actual != tc.expected {
-//			t.Errorf("Unexpected result - input: %s, expected: %s, got: %s", tc.input, tc.expected, actual)
-//		}
-//	}
-//}
+func TestDecodeEmojiUnicode(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{"[\\u1F602]", "😂"},
+		{"[\\u1F60A]", "😊"},
+		{"[\\u1F60E]456", "😎456"},
+		{"No emoji", "No emoji"},
+	}
+
+	for _, tc := range testCases {
+		res := DecodeEmojiUnicode(tc.input)
+
+		// 转大写
+		res = strings.ToUpper(res)
+		expected := strings.ToUpper(tc.expected)
+
+		if res != expected {
+			t.Errorf("Unexpected result - input: %s, expected: %s, got: %s",
+				tc.input,
+				expected,
+				res,
+			)
+		}
+	}
+}
+
+func TestDecodeEmojiUnicode2(t *testing.T) {
+	input := "[\\u1F602]"
+	expected := "😂"
+	result := DecodeEmojiUnicode(input)
+
+	// 转大写
+	expected = strings.ToUpper(expected)
+	result = strings.ToUpper(result)
+
+	if result != expected {
+		t.Errorf("DecodeEmoji(%s) = %s; expected %s", input, result, expected)
+	}
+}
+
+func TestEncodeEmojiUnicode(t *testing.T) {
+	testCases := []struct {
+		input    string
+		expected string
+	}{
+		{"Hello 😂", "Hello [\\u1F602]"},
+		{"No emoji", "No emoji"},
+	}
+
+	for _, tc := range testCases {
+		actual := EncodeEmojiUnicode(tc.input)
+
+		// 转大写
+		actual = strings.ToUpper(actual)
+		expected := strings.ToUpper(tc.expected)
+
+		if actual != expected {
+			t.Errorf("Unexpected result - input: %s, expected: %s, got: %s",
+				tc.input,
+				expected,
+				actual,
+			)
+		}
+	}
+}
+
+func TestEncodeEmojiUnicode2(t *testing.T) {
+	input := "😂"
+	expected := "[\\u1F602]"
+	result := EncodeEmojiUnicode(input)
+
+	// 转大写
+	expected = strings.ToUpper(expected)
+	result = strings.ToUpper(result)
+
+	if result != expected {
+		t.Errorf("EncodeEmoji(%s) = %s; expected %s", input, result, expected)
+	}
+}
