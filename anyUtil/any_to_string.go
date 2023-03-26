e436397995d1e457163fa2a25e7d5a259387cd66
李斌 <libin1-hj@huafang.com>
2023-03-26 16:01:35 +0800

    unit test

--- anyUtil/any_to_string.go
+++ anyUtil/any_to_string.go
@@ -1,40 +1,48 @@
 package anyUtil
 
 import (
+	"fmt"
+	"reflect"
 	"strconv"
 )
 
 // AnyToStr 任意类型数据转string
-func AnyToStr(input interface{}) string {
-	switch v := input.(type) {
-	case string:
-		return v
-	case int:
-		return strconv.Itoa(v)
-	case int8:
-		return strconv.FormatInt(int64(v), 10)
-	case int16:
-		return strconv.FormatInt(int64(v), 10)
-	case int32:
-		return strconv.FormatInt(int64(v), 10)
-	case int64:
-		return strconv.FormatInt(v, 10)
-	case uint:
-		return strconv.FormatUint(uint64(v), 10)
-	case uint8:
-		return strconv.FormatUint(uint64(v), 10)
-	case uint16:
-		return strconv.FormatUint(uint64(v), 10)
-	case uint32:
-		return strconv.FormatUint(uint64(v), 10)
-	case uint64:
-		return strconv.FormatUint(v, 10)
-	case float32:
-		return strconv.FormatFloat(float64(v), 'f', -1, 32)
-	case float64:
-		return strconv.FormatFloat(v, 'f', -1, 64)
-	case bool:
-		return strconv.FormatBool(v)
+func AnyToStr(i interface{}) string {
+	if i == nil {
+		return ""
+	}
+
+	// 检查解引用后的值是否为 nil
+	if reflect.ValueOf(i).Kind() == reflect.Ptr && reflect.ValueOf(i).IsNil() {
+		return ""
+	}
+
+	v := reflect.ValueOf(i)
+	// 处理指针类型
+	if reflect.TypeOf(i).Kind() == reflect.Ptr {
+		if reflect.ValueOf(i).IsNil() {
+			return ""
+		}
+		v = reflect.ValueOf(i).Elem()
+	}
+
+	switch v.Kind() {
+	case reflect.String:
+		return v.String()
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return strconv.FormatInt(v.Int(), 10)
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return strconv.FormatUint(v.Uint(), 10)
+	case reflect.Float32:
+		return strconv.FormatFloat(v.Float(), 'f', -1, 32)
+	case reflect.Float64:
+		return strconv.FormatFloat(v.Float(), 'f', -1, 64)
+	case reflect.Complex64:
+		return fmt.Sprintf("(%g+%gi)", real(v.Complex()), imag(v.Complex()))
+	case reflect.Complex128:
+		return fmt.Sprintf("(%g+%gi)", real(v.Complex()), imag(v.Complex()))
+	case reflect.Bool:
+		return strconv.FormatBool(v.Bool())
 	default:
 		return ""
 	}
--- anyUtil/any_to_uint_x.go
+++ anyUtil/any_to_uint_x.go
@@ -59,30 +59,65 @@ func AnyToUint32(input interface{}) (uint32, error) {
 }
 
 // AnyToUint64 将给定的值转换为 uint64
-func AnyToUint64(value interface{}) (uint64, error) {
-	switch reflect.TypeOf(value).Kind() {
-	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64:
-		return reflect.ValueOf(value).Uint(), nil
+func AnyToUint64(i interface{}) (uint64, error) {
+	if i == nil {
+		return 0, nil
+	}
+
+	// 检查解引用后的值是否为 nil
+	if reflect.ValueOf(i).Kind() == reflect.Ptr && reflect.ValueOf(i).IsNil() {
+		return 0, nil
+	}
+
+	v := reflect.ValueOf(i)
+	// 处理指针类型
+	if reflect.TypeOf(i).Kind() == reflect.Ptr {
+		if reflect.ValueOf(i).IsNil() {
+			return 0, nil
+		}
+		v = reflect.ValueOf(i).Elem()
+	}
+
+	switch v.Kind() {
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint(), nil
 	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
-		v := reflect.ValueOf(value).Int()
-		if v < 0 {
-			return 0, go_easy_utils.ErrUnsignedInt
+		intValue := v.Int()
+		if intValue < 0 {
+			return 0, nil
 		}
-		return uint64(v), nil
+		return uint64(intValue), nil
 	case reflect.Float32, reflect.Float64:
-		v := reflect.ValueOf(value).Float()
-		if v < 0 {
-			return 0, go_easy_utils.ErrUnsignedInt
+		floatValue := v.Float()
+		if floatValue < 0 {
+			return 0, nil
 		}
-		return uint64(v), nil
+		return uint64(floatValue), nil
+	case reflect.Complex64, reflect.Complex128:
+		realValue := real(v.Complex())
+		if realValue < 0 {
+			return 0, nil
+		}
+		return uint64(realValue), nil
 	case reflect.String:
-		val, err := strconv.ParseUint(value.(string), 10, 64)
+		strValue := v.String()
+		uintValue, err := strconv.ParseUint(strValue, 10, 64)
 		if err != nil {
 			return 0, go_easy_utils.ErrSyntax
 		}
-		return val, nil
-	case reflect.Interface:
-		return AnyToUint64(reflect.ValueOf(value).Elem().Interface())
+		return uintValue, nil
+	case reflect.Bool:
+		if v.Bool() {
+			return 1, nil
+		} else {
+			return 0, nil
+		}
+	//case reflect.Ptr, reflect.Interface:
+	//	if v.IsNil() {
+	//		return 0, nil
+	//	}
+	//	return toUint64(v.Elem().Interface())
+	default:
+		return 0, go_easy_utils.ErrType
 	}
-	return 0, go_easy_utils.ErrType
 }
