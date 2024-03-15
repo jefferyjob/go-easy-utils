d5e042f4b976cb7d1d49e05114ab79bd4814ac22
libin <58850169@qq.com>
2024-03-15 13:33:34 +0800

    fix: Conflict Naming Modification (#63)

--- mathUtil/rand_test.go
+++ mathUtil/rand_test.go
@@ -3,12 +3,12 @@ package mathUtil
 import "testing"
 
 func TestRand(t *testing.T) {
-	min := 1
-	max := 100
+	minNum := 1
+	maxNum := 100
 	for i := 0; i < 1000; i++ {
-		num := Rand(min, max)
-		if num < min || num > max {
-			t.Errorf("randRange() returned %d, which is outside the range of [%d, %d]", num, min, max)
+		num := Rand(minNum, maxNum)
+		if num < minNum || num > maxNum {
+			t.Errorf("randRange() returned %d, which is outside the range of [%d, %d]", num, minNum, maxNum)
 		}
 	}
 }
