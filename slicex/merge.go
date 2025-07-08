393716f46ee012d6b2b690c6aa021f3e7008173c
libin <libin@zmexing.com>
2025-07-08 15:14:56 +0800

    fix

--- slicex/merge.go
+++ slicex/merge.go
@@ -2,11 +2,11 @@ package slicex
 
 // Merge 将多个slice合并成一个slice
 func Merge[T any](slices ...[]T) []T {
-	var new []T
+	var s []T
 	for _, slice := range slices {
 		for _, v := range slice {
-			new = append(new, v)
+			s = append(s, v)
 		}
 	}
-	return new
+	return s
 }
